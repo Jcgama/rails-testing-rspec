92b2869ed30bddbd9214ab7eb13be3ec20474dbe
Julio Cezar <juliogama@live.com>
2017-03-22 18:18:23 +0000

    Add factory_girl, capybara and faker

---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe ProductsController do
+    describe "GET #index" do
+        it "populates an array of products"
+        it "renders the :index template"
+    end
+    
+    describe "GET #new" do
+       it "assigns a new product"
+       it "renders the :new template"
+    end
+    
+    describe "POST #create" do
+    end
+    
+    
+end
